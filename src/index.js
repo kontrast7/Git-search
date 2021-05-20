b3eaf4132d64850e28bbd8d2c2700b8d3142ef66
kontrast7 <maks.mashko@gmail.com>
2021-05-20 12:35:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/App/app.js";
+import App from "./Components/App/App.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
